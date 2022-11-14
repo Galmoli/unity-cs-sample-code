ae3442016ad3ec76f71b8eadc4946bd95eaa1aaf
Toni Galmés Oliver <tonigalmes98@gmail.com>
2022-11-14 11:24:41 +0100

    Add getter and setter for playfab

---
+++ Code/ApplicationLayer/Services/Server/PlayFab/PlayFabGetUserDataService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using ApplicationLayer.Services.Server.Gateways.ServerData;
+using Cysharp.Threading.Tasks;
+using PlayFab;
+using PlayFab.ClientModels;
+using SystemUtilities;
+
+namespace ApplicationLayer.Services.Server.PlayFab
+{
+    public class PlayFabGetUserDataService : IGetDataService
+    {
+        public UniTask<Optional<DataResult>> Get(List<string> keys)
+        {
+            var taskCompletionSource = new UniTaskCompletionSource<Optional<DataResult>>();
+
+            RequestUserData(keys, taskCompletionSource);
+
+            return UniTask.Create(() => taskCompletionSource.Task);
+        }
+
+        private void OnSuccess(GetUserDataResult result, UniTaskCompletionSource<Optional<DataResult>> t)
+        {
+            var resultData = new Dictionary<string, string>(result.Data.Count);
+            foreach (var userDataRecord in result.Data)
+            {
+                resultData.Add(userDataRecord.Key, userDataRecord.Value.Value);
+            }
+
+            t.TrySetResult(new Optional<DataResult>(new DataResult(resultData)));
+        }
+
+        private void OnError(PlayFabError error, UniTaskCompletionSource<Optional<DataResult>> t)
+        {
+            t.TrySetResult(null);
+            throw new Exception(error.ErrorMessage);
+        }
+
+        private void RequestUserData(List<string> keys, UniTaskCompletionSource<Optional<DataResult>> taskCompletionSource)
+        {
+            var request = new GetUserDataRequest {Keys = keys};
+            PlayFabClientAPI.GetUserData(request,
+                result => OnSuccess(result, taskCompletionSource),
+                error => OnError(error, taskCompletionSource));
+        }
+    }
+}
---
+++ Code/ApplicationLayer/Services/Server/PlayFab/PlayFabSetUserDataService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using ApplicationLayer.Services.Server.Gateways.ServerData;
+using Cysharp.Threading.Tasks;
+using Domain.Services.Server;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+namespace ApplicationLayer.Services.Server.PlayFab
+{
+    public class PlayFabSetUserDataService : ISetDataService
+    {
+        public UniTask Set(Dictionary<string, string> _data)
+        {
+            var taskCompletionSource = new UniTaskCompletionSource<bool>();
+            
+            SetUserData(_data, taskCompletionSource);
+            
+            return UniTask.Create(() => taskCompletionSource.Task);
+        }
+
+        private void OnSuccess(UpdateUserDataResult _result, UniTaskCompletionSource<bool> _t)
+        {
+            _t.TrySetResult(true);
+        }
+
+        private void OnError(PlayFabError _error, UniTaskCompletionSource<bool> t)
+        {
+            t.TrySetResult(false);
+            throw new Exception(_error.ErrorMessage);
+        }
+
+        private void SetUserData(Dictionary<string, string> _data, UniTaskCompletionSource<bool> _taskCompletionSource)
+        {
+            var request = new UpdateUserDataRequest{Data = _data};
+            PlayFabClientAPI.UpdateUserData(request, 
+                _result => OnSuccess(_result, _taskCompletionSource),
+                _error => OnError(_error, _taskCompletionSource));
+        }
+    }
+}
